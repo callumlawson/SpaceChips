7935a16acddde9905e3463e3dc4eafb095eac258
CharlesMicou <charlesmicou@gmail.com>
2014-01-02 23:08:47 +0000

    Added orbity ship.

--- Assets/Resources/ShipChips.cs
+++ Assets/Resources/ShipChips.cs
@@ -18,6 +18,41 @@ internal class BasicChip : ShipChip
     }
 }
 
+internal class OrbitChip : ShipChip
+{
+	public void Setup(Ship ship, World world, Simulation simulation){
+
+		var distWire = new AnalogueWire(simulation);
+		var errorWire = new AnalogueWire(simulation);
+		var distThrustWire = new AnalogueWire(simulation);
+		var constDistWire = new AnalogueWire(simulation);
+		var distScalingWire = new AnalogueWire(simulation);
+		var orbitThrustWire = new AnalogueWire(simulation);
+		var netThrustWire = new AnalogueWire(simulation);
+
+		var bearingWire = new AnalogueWire(simulation);
+		var orbitBearing = new AnalogueWire(simulation);
+		var netBearingWire = new AnalogueWire(simulation);
+
+		new Scanner(simulation, ship, world, distWire, bearingWire);
+
+		//Thrust calculations
+		new AnalogueConstant(simulation, ship, world, 0.5f, constDistWire);
+		new AMinusB(simulation, ship, world, distWire, constDistWire, errorWire);
+		new AnalogueConstant(simulation, ship, world, 0.01f, distScalingWire); 
+		new ATimesB(simulation, ship, world, errorWire, distScalingWire, distThrustWire);
+		new AnalogueConstant(simulation, ship, world, 0.01f, orbitThrustWire);
+		new AAddB(simulation, ship, world, distThrustWire, orbitThrustWire, netThrustWire);
+
+		//Bearing calculations
+		new AnalogueConstant(simulation, ship, world, 90.0f, orbitBearing);
+		new AAddB(simulation, ship, world, orbitBearing, bearingWire, netBearingWire);
+
+		new OmniThruster(simulation, ship, world, netBearingWire, netThrustWire);
+
+	}
+}
+
 internal class PassiveChip : ShipChip
 {
     public void Setup(Ship ship, World world, Simulation simulation) { }
