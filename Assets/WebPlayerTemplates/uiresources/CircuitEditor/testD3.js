e097e2a0feda3866663c1d2786feeb8cfdd85725
Callum Lawson <lawsonathome@gmail.com>
2014-04-07 00:57:14 +0100

    Joint JS!

---
+++ Assets/WebPlayerTemplates/uiresources/CircuitEditor/testD3.js
@@ -0,0 +1,18 @@
+﻿var dataset = [
+              {
+                  "ChipType": "AAddB"
+              },
+              {
+                  "ChipType": "ADividedByB"
+              },
+              {
+                  "ChipType": "AMinusB"
+              }
+];
+
+d3.select("body").selectAll("p")
+    .data(dataset)
+    .enter()
+    .append("p")
+    .text(function (data) { return data.ChipType; })
+    .style("color", "yellow");
---
+++ Assets/WebPlayerTemplates/uiresources/CircuitEditor/testJoint.js
@@ -0,0 +1,37 @@
+﻿var graph = new joint.dia.Graph;
+
+var paper = new joint.dia.Paper({
+    el: $('body'),
+    width: 1000,
+    height: 500,
+    model: graph
+});
+
+var rect = new joint.shapes.basic.Rect({
+    position: { x: 100, y: 30 },
+    size: { width: 100, height: 30 },
+    attrs: { rect: { fill: 'blue' }, text: { text: 'my box', fill: 'white' } }
+});
+
+var rect2 = rect.clone();
+rect2.translate(300);
+
+var link = new joint.dia.Link({
+    source: { id: rect.id },
+    target: { id: rect2.id }
+});
+
+var m1 = new joint.shapes.devs.Model({
+    position: { x: 50, y: 50 },
+    size: { width: 90, height: 90 },
+    inPorts: ['A', 'B'],
+    outPorts: ['C'],
+    attrs: {
+        '.label': { text: 'AND', 'ref-x': .4, 'ref-y': .2 },
+        rect: { fill: '#2ECC71' },
+        '.inPorts circle': { fill: '#16A085' },
+        '.outPorts circle': { fill: '#E74C3C' }
+    }
+});
+
+graph.addCells([rect, rect2, link, m1]);
